a1ba26eb4016834cb3a018c30acdd53d37e5a7e9
David Torrubia <davidtorrubiasantos@gmail.com>
2024-03-21 17:53:34 +0100

    Third Implementation

---
+++ Route.cs
@@ -0,0 +1,11 @@
+class Route
+{
+    public string FirstCity { get; set; }
+    public string SecondCity { get; set; }
+    public int Distance { get; set; }
+    public int Price { get; set; }
+    public Route()
+    {
+        
+    }
+}

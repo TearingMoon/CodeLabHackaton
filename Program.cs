c613c6b58914a170044828946000ff2b705f8baf
David Torrubia <davidtorrubiasantos@gmail.com>
2024-03-21 18:28:45 +0100

    Implementated gitignore

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿
-
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
 string routeFile = "./assets/rutas.txt";    
 
 List<Route> routes = fileReader.GetRouteList(routeFile);
 fileReader.PrintRouteList(routes);
+
